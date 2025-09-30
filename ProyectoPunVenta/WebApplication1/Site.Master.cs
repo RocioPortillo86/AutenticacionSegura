5e0ff392ae12151f631057d325165c9f83248d4b
RocioPortillo86 <rocioportillo23@gmail.com>
2025-09-30 11:21:22 -0700

    Add files via upload

---
+++ ProyectoPunVenta/WebApplication1/Site.Master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.Class.Services;
+
+namespace WebApplication1
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*if (Session["uid"] != null)
+            {
+                pnlAdmin.Visible = Session["role"].ToString() == "Admin";
+            }
+            else
+            {
+                pnlAdmin.Visible = false;
+            }*/
+        }
+
+        protected void lnkLogout_Click(object sender, EventArgs e)
+        {
+            var authService = new AuthService();
+            authService.Logout(Session);
+            Response.Redirect("~/Pages/Login.aspx");
+        }
+    }
+}
